6a8d3c1062ffcec8cfbe7ae28714b195b237208c
Baskar <baskarvp17@gmail.com>
2020-05-18 23:19:22 +0530

    Updates

---
+++ precache-manifest.d5437c7879173c4a42a96b6b143a1da7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8faff7ee8377878babf419ebfa13ad79",
+    "url": "/reactProject/index.html"
+  },
+  {
+    "revision": "804860da44c69a8681e8",
+    "url": "/reactProject/static/css/2.a2732ee9.chunk.css"
+  },
+  {
+    "revision": "966a44bb2931c59481b3",
+    "url": "/reactProject/static/css/main.8b4ac118.chunk.css"
+  },
+  {
+    "revision": "804860da44c69a8681e8",
+    "url": "/reactProject/static/js/2.62e8096a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactProject/static/js/2.62e8096a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "966a44bb2931c59481b3",
+    "url": "/reactProject/static/js/main.bb1732c4.chunk.js"
+  },
+  {
+    "revision": "83e17823918365faf2d9",
+    "url": "/reactProject/static/js/runtime-main.e6d000b0.js"
+  },
+  {
+    "revision": "c41f6908a60ff346d5162c0f13db92f2",
+    "url": "/reactProject/static/media/em.c41f6908.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactProject/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactProject/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactProject/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactProject/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactProject/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
